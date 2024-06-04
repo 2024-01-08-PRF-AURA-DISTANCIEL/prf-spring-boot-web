c8c2adafc7a7382d024d546a5736ced29180104b
François Caremoli <francois.caremoli@wanadoo.fr>
2024-06-04 10:07:58 +0200

    Ajout get dans Controller Company

--- src/main/java/fr/maboite/demo/spring/boot/dao/CompanyDao.java
+++ src/main/java/fr/maboite/demo/spring/boot/dao/CompanyDao.java
@@ -1,9 +1,11 @@
 package fr.maboite.demo.spring.boot.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.maboite.demo.spring.boot.model.Company;
 
+@Repository
 public interface CompanyDao extends CrudRepository<Company, Integer> {
 
 }
--- src/main/java/fr/maboite/demo/spring/boot/model/Company.java
+++ src/main/java/fr/maboite/demo/spring/boot/model/Company.java
@@ -10,22 +10,25 @@ import jakarta.persistence.Id;
  */
 @Entity // Décrit une entité persistable
 public class Company {
-	
+
 	@Id // Décrit l’attribut servant d'identifiant et la clé primaire
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // décrit la méthode de génération de l’identifiant
 	private Integer id;
-	
+
 	private String name;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
